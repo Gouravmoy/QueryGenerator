4fb450f288a8689e2910b8c9f14191a2ae6296c0
GMohanty <gouravmoy.mohanty91@gmail.com>
2016-03-01 20:39:24 +0530

    Fetch MetaData implemented

--- src/com/controller/Controller.java
+++ src/com/controller/Controller.java
@@ -5,7 +5,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
+import com.entity.Column;
 import com.entity.Tables;
 import com.extra.Keys;
 import com.util.DBUtil;
@@ -53,6 +55,49 @@ public class Controller {
 	public static ArrayList<Tables> getTablesMetaInfo(
 			ArrayList<String> selectedTableNames) {
 		ArrayList<Tables> tables = new ArrayList<Tables>();
+		ArrayList<Column> columns = new ArrayList<Column>();
+		Column column = new Column();
+		Tables table = new Tables();
+		Connection conn = null;
+		PreparedStatement preparedStatement;
+		ResultSet res;
+
+		try {
+			String sql = MasterCommon.queriesProps
+					.getProperty(Keys.KEY_SQL_META_QUERY);
+			System.out.println("Connecting to Database");
+			conn = DBUtil.getSQLConnection();
+			System.out.println("Connected to Database");
+			preparedStatement = conn.prepareStatement(sql);
+			for (String tableName : selectedTableNames) {
+				columns = new ArrayList<>();
+				preparedStatement.setString(1, tableName);
+				preparedStatement.setString(2, MasterCommon.schemaName);
+				res = preparedStatement.executeQuery();
+				table = new Tables();
+				table.setTableName(tableName);
+				while (res.next()) {
+					column = new Column();
+					column.setColName(res.getString(1));
+					column.setDataType(res.getString(2));
+					columns.add(column);
+				}
+				table.setColumnList(columns);
+				tables.add(table);
+			}
+			preparedStatement.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 		return tables;
 	}
 
