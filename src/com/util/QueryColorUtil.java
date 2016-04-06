9d9240a0b449ed3535d738e5aa5bbf0e64b0d24e
GMohanty <gouravmoy.mohanty91@gmail.com>
2016-04-06 19:10:16 +0530

    Defect 5 Closed

--- src/com/util/QueryColorUtil.java
+++ src/com/util/QueryColorUtil.java
@@ -4,66 +4,71 @@ import com.controller.MasterCommon;
 
 public class QueryColorUtil extends MasterCommon {
 
-	public static String queryColorChange(String query) {
+       public static String queryColorChange(String query) {
+              System.out.println("query" + query);
 
-		String[] queryParts = query.split("\n");
-		String finalQuery = "";
+              String[] queryParts = query.split("\n");
+              String finalQuery = "";
 
-		String keyWords = keywordsProps.getProperty("SQL_KEYWORDS");
-		String[] parts = keyWords.split(",");
-		for (int j = 0; j < queryParts.length; j++) {
-			String Query = "";
-			int flag = 0;
-			String[] queryPartsSplit = queryParts[j].split(" ");
-			for (int k = 0; k < queryPartsSplit.length; k++) {
+              String keyWords = keywordsProps.getProperty("SQL_KEYWORDS");
+              String[] parts = keyWords.split(",");
+              for (int j = 0; j < queryParts.length; j++) {
+                     String Query = "";
+                     int flag = 0;
+                     String[] queryPartsSplit = queryParts[j].split(" ");
+                     for (int k = 0; k < queryPartsSplit.length; k++) {
 
-				int count = 0;
+                           int count = 0;
 
-				for (int i = 0; i < parts.length; i++) {
-					/*
-					 * if (parts[i].equals("SELECT")) {
-					 * System.out.println("BBKBK"); }
-					 */
-					if ((queryPartsSplit[k].trim()).equalsIgnoreCase(parts[i])) {
+                           for (int i = 0; i < parts.length; i++) {
+                                  if (queryPartsSplit[k].equalsIgnoreCase("'aaa'")) {
+                                         System.out.println(queryPartsSplit[k]);
+                                  }
+                                  /*
+                                  * if (parts[i].equals("SELECT")) {
+                                  * System.out.println("BBKBK"); }
+                                  */
+                                  if ((queryPartsSplit[k].trim()).equalsIgnoreCase(parts[i])) {
 
-						queryPartsSplit[k] = "<font size=\"3\" face=\"verdana\" color=\"blue\">"
-								+ queryPartsSplit[k] + "</font>";
-						Query = Query + queryPartsSplit[k] + " ";
+                                         queryPartsSplit[k] = "<font size=\"3\" face=\"verdana\" color=\"blue\">"
+                                                       + queryPartsSplit[k] + "</font>";
+                                         Query = Query + queryPartsSplit[k] + " ";
 
-						count++;
-						break;
+                                         count++;
+                                         break;
 
-					} else if ((queryPartsSplit[k].startsWith("'"))) {
+                                  } else if ((queryPartsSplit[k].startsWith("'"))) {
 
-						count++;
-						flag++;
-						break;
+                                         count++;
+                                         flag++;
+                                         break;
 
-					}
-				}
-				if (flag == 1) {
-					if (queryPartsSplit[k].endsWith("'")) {
-						flag = 0;
-						break;
-					}
-					queryPartsSplit[k] = "<font size=\"3\" face=\"verdana\" color=\"green\">"
-							+ queryPartsSplit[k] + "</font>";
-					Query = Query + queryPartsSplit[k] + " ";
+                                  }
+                           }
+                           if (flag == 1) {
+                                  if (queryPartsSplit[k].endsWith("'")) {
+                                         flag = 0;
 
-					count++;
+                                  }
+                                  queryPartsSplit[k] = "<font size=\"3\" face=\"verdana\" color=\"green\">"
+                                                + queryPartsSplit[k] + "</font>";
+                                  Query = Query + queryPartsSplit[k] + " ";
 
-				} else if (count == 0) {
-					Query = Query + queryPartsSplit[k] + " ";
-				}
-			}
+                                  count++;
 
-			finalQuery = finalQuery + Query + "<br>";
+                           } else if (count == 0) {
+                                  Query = Query + queryPartsSplit[k] + " ";
+                           }
+                     }
 
-		}
-		System.out.println(finalQuery);
-		finalQuery = "<html>" + finalQuery + "</html>";
-		// System.out.println(finalQuery);
-		return finalQuery;
+                     finalQuery = finalQuery + Query + "<br>";
 
-	}
+              }
+              System.out.println(finalQuery);
+              finalQuery = "<html>" + finalQuery + "</html>";
+              // System.out.println(finalQuery);
+              return finalQuery;
+
+       }
 }
+
