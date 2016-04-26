71b6bd5bc85a1e7a9478b0d7804f75356a74a91e
Gouravmoy Mohanty <gouravmoy.mohanty91@gmail.com>
2016-04-27 01:34:55 +0530

    Auto Suggest Started

---
+++ src/com/test/AutoSuggestInnerJoin.java
@@ -0,0 +1,69 @@
+package com.test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AutoSuggestInnerJoin {
+
+	public static void main(String args[]) {
+		String currline;
+		File file = new File("C:/Users/lenovo/Downloads/sakila-db/FK.txt");
+		List<Table> tables = null;
+		List<String> POK;
+		List<String> FOK;
+		Table table;
+		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+			String[] split;
+			tables = new ArrayList<>();
+			while ((currline = reader.readLine()) != null) {
+				split = currline.split("\\|");
+				table = ifTableExists(tables, split);
+				if (table == null) {
+					table = new Table();
+					table.setTableName(split[0]);
+					if ("NULL".equals(split[2])) {
+						POK = new ArrayList<>();
+						POK.add(split[1]);
+						table.setPrimarykeys(POK);
+					} else {
+						FOK = new ArrayList<>();
+						FOK.add(split[1] + "|" + split[2] + "|" + split[3]);
+						table.setForeignKeys(FOK);
+					}
+					tables.add(table);
+				} else {
+					if ("NULL".equals(split[2])) {
+						POK = table.getPrimarykeys();
+						if (POK == null)
+							POK = new ArrayList<>();
+						POK.add(split[1]);
+						table.setPrimarykeys(POK);
+					} else {
+						FOK = table.getForeignKeys();
+						if (FOK == null)
+							FOK = new ArrayList<>();
+						FOK.add(split[1] + "|" + split[2] + "|" + split[3]);
+						table.setForeignKeys(FOK);
+					}
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println(tables);
+	}
+
+	private static Table ifTableExists(List<Table> tables, String[] split) {
+		for (Table table : tables) {
+			if (table.getTableName().equals(split[0])) {
+				return table;
+			}
+		}
+		return null;
+	}
+
+}
---
+++ src/com/test/Table.java
@@ -0,0 +1,40 @@
+package com.test;
+
+import java.util.List;
+
+public class Table {
+
+	String tableName;
+	List<String> primarykeys;
+	List<String> foreignKeys;
+
+	@Override
+	public String toString() {
+		return "Table [tableName=" + tableName + ", primarykeys=" + primarykeys + ", foreignKeys=" + foreignKeys + "]";
+	}
+
+	public String getTableName() {
+		return tableName;
+	}
+
+	public void setTableName(String tableName) {
+		this.tableName = tableName;
+	}
+
+	public List<String> getPrimarykeys() {
+		return primarykeys;
+	}
+
+	public void setPrimarykeys(List<String> primarykeys) {
+		this.primarykeys = primarykeys;
+	}
+
+	public List<String> getForeignKeys() {
+		return foreignKeys;
+	}
+
+	public void setForeignKeys(List<String> foreignKeys) {
+		this.foreignKeys = foreignKeys;
+	}
+
+}
