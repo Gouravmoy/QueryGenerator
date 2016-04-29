e9843fc5a1b2c8ab328684c141db6e2fb81145a9
Gouravmoy Mohanty <gouravmoy.mohanty91@gmail.com>
2016-04-29 12:24:36 +0530

    Home commit

--- src/com/service/Tesla2Functions.java
+++ src/com/service/Tesla2Functions.java
@@ -110,79 +110,9 @@ public class Tesla2Functions extends Tesla2 {
 						MasterCommon.completeQuery = MasterCommon.completeQuery + r.getTable().getTableName() + "."
 								+ r.getTable().getColumn().getColumnName() + ", \n";
 					}
+				}
 
-				} else if (r.getRowType().equals("Case")) {
-					String caseQuery = "Case \n";
-					for (int j = 0; j < r.getCaseRow().size(); j++) {
-						String value = "";
-						CaseRow r1 = r.getCaseRow().get(j);
-						if (r1.getTableOne().getTableName() == null) {
-							r.getCaseRow().remove(j);
-						} else {
-							if (r1.getConditionString().equals("ELSE")) {
-								if (r1.getValueString().length() == 0) {
-									value = r1.getTableTwo().getTableName() + "."
-											+ r1.getTableTwo().getColumn().getColumnName() + " \n";
-								} else {
-									value = "'" + r1.getValueString() + "' \n";
-								}
-								caseQuery = caseQuery + " ELSE " + value;
-
-							} else {
-								if (r1.getValueString().length() == 0) {
-									value = r1.getTableTwo().getTableName() + "."
-											+ r1.getTableTwo().getColumn().getColumnName() + " \n";
-								} else {
-									value = "'" + r1.getValueString() + "' \n";
-								}
-								caseQuery = caseQuery + " When " + r1.getTableOne().getTableName() + "."
-										+ r1.getTableOne().getColumn().getColumnName() + " = '"
-										+ r1.getConditionString() + "' Then " + value;
-							}
-						}
-					}
-
-					// caseQuery = caseQuery + " End Case \n as '" +
-					// r.getElementname() + "' , \n";
-					caseQuery = caseQuery + " End \n as '" + r.getElementname() + "' , \n";// My
-																							// SQL
-																							// Syntax
-					MasterCommon.completeQuery = MasterCommon.completeQuery + caseQuery;
-				} else if (r.getRowType().equals("Coalesce")) {
-					String coalesceQuery = "COALESCE (";
-					for (int k = 0; k < r.getCoalesceRow().size(); k++) {
-						String tableColString = "";
-						CoalesceRow r2 = r.getCoalesceRow().get(k);
-						tableColString += r2.getTableOne().getTableName() + "."
-								+ r2.getTableOne().getColumn().getColumnName();
-						if (!r2.getStringValue().equals("")) {
-							coalesceQuery += r2.getStringValue().replace("#", tableColString);
-						} else {
-							coalesceQuery += tableColString;
-						}
-						coalesceQuery += ",";
-					}
-					if (!r.getCoalesceString().equals("")) {
-						coalesceQuery += "'" + r.getCoalesceString() + "'),";
-					} else {
-						coalesceQuery = coalesceQuery.substring(0, coalesceQuery.length() - 1);
-						coalesceQuery += "),";
-					}
-					MasterCommon.completeQuery = MasterCommon.completeQuery + coalesceQuery;
-					String tableColumn = "";
-					tableColumn = r.getTable().getTableName() + "." + r.getTable().getColumn().getColumnName();
-					if (r.getConditionString().equals("")) {
-						MasterCommon.completeQuery += tableColumn;
-					} else {
-						MasterCommon.completeQuery += r.getConditionString().replace("#", tableColumn);
-					}
-					if (r.getElementname().equals("")) {
-						MasterCommon.completeQuery += " , \n";
-					} else {
-						MasterCommon.completeQuery += " as '" + r.getElementname() + "' ,\n";
-					}
-
-				} else if (r.getRowType().equals("Case")) {
+				 else if (r.getRowType().equals("Case")) {
 					String query = TeslaTransFunctions.displayCaseQuery(r.getCaseRow());
 					MasterCommon.completeQuery = MasterCommon.completeQuery + query + " as '" + r.getElementname()
 							+ "' , \n ";
